af477c29f22e022f1885bb10d15e23149175f0f5
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-10-21 19:27:18 -0500

    first commit

--- Controllers/ControladorReservas.js
+++ Controllers/ControladorReservas.js
@@ -47,60 +47,90 @@ export class ControladorReservas{
      
     async registrarReserva(request,response){
         let objthelp= new Helpers
-        let objetoServicioHabitacion = new ServicioHAbitacion()
-        let objserviciorecerva = new ServicioReserva
         let datosReserva = request.body
-        objthelp.validar(datosReserva)
-            
-        try{
-            let objhabitacion = await objetoServicioHabitacion.buscarHabitacionPorId(datosReserva.idhabitacion)
-            if(objhabitacion!={}){
-                let cantidadDiasReservados=(Date.parse(datosReserva.fechaSalida)-Date.parse(datosReserva.fechaEntrada))/60/60/24000
-                if(cantidadDiasReservados>0){
-                    let cantidadPersonas=datosReserva.numeroAdultos+datosReserva.numeroNinos
-                    if(cantidadPersonas>0&&cantidadPersonas<=objhabitacion.numeroMaximoPersonas){
-                        let costoReserva=objhabitacion.valorNoche*cantidadDiasReservados;
-                        datosReserva.costoreserva=costoReserva
-                        await objserviciorecerva.guardarReserva(datosReserva)
-                        response.status(200).json({
-
-                            "mensaje":"Exito en el registro", 
-                            "datos": datosReserva
-                        })
-
-                    }else{
-                        response.status(403).json({
-
-                            "mensaje":"La habitacion no puede alvergar mas de "+objhabitacion.numeroMaximoPersonas+" personas", 
-                            "datos": null
-                        })
-                    }
-                }else{
-                    response.status(402).json({
-
-                        "mensaje":"Revise las fechas ingresadas. debe recervar de 1 dia en adelante", 
-                        "datos": null
-                    })
-                }
-            
-            
-            }else{
-                response.status(401).json({
-
-                    "mensaje":"La habitacion no existe", 
-                    "datos": objhabitacion
-                })
-            }
-            
-           
-            
-        }catch(error){
-
-            response.status(400).json({
+        
+        
+        let objserviciorecerva = new ServicioReserva
+        
+        let r= await objthelp.validar(datosReserva)
+        
+         console.log(r)
+       // let objetoServicioHabitacion = new ServicioHAbitacion()
+        if(r.estado==true){
+                objserviciorecerva.guardarReserva(r)
+            response.status(200).json({
+                "mensaje": "Exito",
+                "datos" : r.info
+    
+            })
+        }else if(r.info=="p"){
+            response.status(403).json({
+                "mensaje":"La habitacion no puede alvergar mas de "+r.estado+" personas", 
+                "datos": null
+    
+            })
+        } else if(r.info=="d"){
+            response.status(402).json({
+                "mensaje":"Revise las fechas ingresadas. debe recervar de 1 noche en adelante", 
+                "datos": null
+    
+            })
+        }else {
+            response.status(400).json({ 
                 "mensaje":"Error en la consulta"+error, 
                 "datos":null
             })
         }
+        // try{
+        //     let objhabitacion = await objetoServicioHabitacion.buscarHabitacionPorId(datosReserva.idhabitacion)
+        //     if(objhabitacion!={}){
+        //         let cantidadDiasReservados=(Date.parse(datosReserva.fechaSalida)-Date.parse(datosReserva.fechaEntrada))/60/60/24000
+        //         if(cantidadDiasReservados>0){
+        //             let cantidadPersonas=datosReserva.numeroAdultos+datosReserva.numeroNinos
+        //             if(cantidadPersonas>0&&cantidadPersonas<=objhabitacion.numeroMaximoPersonas){
+        //                 let costoReserva=objhabitacion.valorNoche*cantidadDiasReservados;
+        //                 datosReserva.costoreserva=costoReserva
+        //                 await objserviciorecerva.guardarReserva(datosReserva)
+                        
+        //                 response.status(200).json({
+
+        //                     "mensaje":"Exito en el registro", 
+        //                     "datos": datosReserva
+        //                 })
+
+        //             }else{
+        //                 response.status(403).json({
+
+        //                     "mensaje":"La habitacion no puede alvergar mas de "+objhabitacion.numeroMaximoPersonas+" personas", 
+        //                     "datos": null
+        //                 })
+        //             }
+        //         }else{
+        //             response.status(402).json({
+
+        //                 "mensaje":"Revise las fechas ingresadas. debe recervar de 1 dia en adelante", 
+        //                 "datos": null
+        //             })
+        //         }
+            
+            
+        //     }else{
+        //         response.status(401).json({
+
+        //             "mensaje":"La habitacion no existe", 
+        //             "datos": objhabitacion
+        //         })
+        //     }
+            
+           
+            
+        // }catch(error){
+
+        //     response.status(400).json({
+        //         "mensaje":"Error en la consulta"+error, 
+        //         "datos":null
+        //     })
+        // }
        // response.send("estoy agregando desde el controlador")
     }
 
@@ -108,21 +138,36 @@ export class ControladorReservas{
         let objserviciorecerva = new ServicioReserva
         let id=request.params.idReserva
         let datosEditar = request.body
-        console.log(id,datosEditar)
-        try{
-            await objserviciorecerva.editarReserva(id,datosEditar)
+        
+        let r= await objthelp.validar(datosEditar)
+        
+         console.log(r)
+       // let objetoServicioHabitacion = new ServicioHAbitacion()
+        if(r.estado==true){
+                objserviciorecerva.editarReserva(id,r)
             response.status(200).json({
-
-                "mensaje":"Exito Editando a: "+id, 
-                "datos": datosEditar
+                "mensaje": "Exito",
+                "datos" : r.info
+    
             })
-        }catch(error){
-
-            response.status(400).json({
+        }else if(r.info=="p"){
+            response.status(403).json({
+                "mensaje":"La habitacion no puede alvergar mas de "+r.estado+" personas", 
+                "datos": null
+    
+            })
+        } else if(r.info=="d"){
+            response.status(402).json({
+                "mensaje":"Revise las fechas ingresadas. debe recervar de 1 noche en adelante", 
+                "datos": null
+    
+            })
+        }else {
+            response.status(400).json({ 
                 "mensaje":"Error en la consulta"+error, 
-                "datos":null,
+                "datos":null
             })
-        }
+        
        // response.send("estoy editando desde el controlador")
     }
     async eliminarReserva(request,response){
@@ -143,8 +188,5 @@ export class ControladorReservas{
         }
 
     }
-    validarDatos(data){
-    console.log(data)
-
-    }
+    
 }
--- helpers/Helpers.js
+++ helpers/Helpers.js
@@ -1,6 +1,30 @@
-
+import { ServicioHAbitacion } from "../services/ServicioHabitacion.js"
 export class Helpers {
-    validar(data){
-        console.log(data)
+    async validar(datosReserva){
+        let objetoServicioHabitacion = new ServicioHAbitacion()
+        try{
+            let objhabitacion = await objetoServicioHabitacion.buscarHabitacionPorId(datosReserva.idhabitacion)
+
+                let cantidadDiasReservados=(Date.parse(datosReserva.fechaSalida)-Date.parse(datosReserva.fechaEntrada))/60/60/24000
+                if(cantidadDiasReservados>0){
+                    let cantidadPersonas=datosReserva.numeroAdultos+datosReserva.numeroNinos
+                    if(cantidadPersonas>0&&cantidadPersonas<=objhabitacion.numeroMaximoPersonas){
+                        let costoReserva=objhabitacion.valorNoche*cantidadDiasReservados;
+                        datosReserva.costoreserva=costoReserva
+                        //await objserviciorecerva.guardarReserva(datosReserva)
+                        
+                        return {"estado":true, "info":datosReserva}
+
+                    }else{
+                       return {"estado":objhabitacion.numeroMaximoPersonas, "info":"p"}
+                    }
+                }else{
+                    return {"estado":false, "info":"d"}
+                }
+            
+        }catch(error){
+
+            return error
+        }
     }
 }
